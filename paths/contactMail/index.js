380e7072f211b1fa93ee118ff51d886d51d21ea8
Michał Łysakowski <lysakowskimichal@outlook.com>
2020-11-24 17:51:35 +0100

    update

--- paths/contactMail/index.js
+++ paths/contactMail/index.js
@@ -4,20 +4,21 @@ module.exports = (req,res) => {
     const {mailContent, senderMail} = req.body
     if(mailContent && senderMail) {
         const mailOptions = {
-            from: 'no-reply@cruxofpoland.pl',
+            from: "no-reply@cruxofpoland.pl",
             to: 'admin@cruxofpoland.pl',
             subject: `Question from: ${senderMail}`,
-            // text: 'Hello '+ user.userName +',\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + user.emailAdress + '\/' + token.token + '\n\nThank You!\n'
-            text: mailContent
+            text: `${mailContent}`
         };
         sgMail.setApiKey(process.env.SENDGRID_KEY)
-        sgMail.send(mailOptions).then((res)=>{
-            console.log(res)
-        }).catch(err=>{
-            res.status(400).send({
-                message: err
-            })
+        sgMail.send(mailOptions)
+            .then((res)=>{
+                console.log(res)
+            }).catch(err=>{
+                res.status(400).send({
+                    message: err
+                })
         })
+        res.status(202).send({message: `Message send`})
     } else {
         res.status(406).status({
             message: `Content or email adress is missing`
